ba89933d0aec07284e1208aca1cf4e65acebcee6
Tsubakurouuu <yzpolice2896@icloud.com>
2023-02-13 15:28:12 +0900

    Spring Freamwork超入門 ~やさしくわかるWEBアプリ開発~ 7章 7-3

--- src/main/java/com/example/controller/RequestParamController.java
+++ src/main/java/com/example/controller/RequestParamController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.example.form.Form;
+
 @Controller
 public class RequestParamController {
 	
@@ -17,12 +19,17 @@ public class RequestParamController {
 		return "entry";
 	}
 	
+//	@PostMapping("confirm")
+//	public String confirmView(Model model, @RequestParam String name, @RequestParam Integer age, @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) @RequestParam LocalDate birth) {
+//		model.addAttribute("name", name);
+//		model.addAttribute("age", age);
+//		model.addAttribute("birth", birth);
+//		return "confirm";
+//	}
+	
 	@PostMapping("confirm")
-	public String confirmView(Model model, @RequestParam String name, @RequestParam Integer age, @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) @RequestParam LocalDate birth) {
-		model.addAttribute("name", name);
-		model.addAttribute("age", age);
-		model.addAttribute("birth", birth);
-		return "confirm";
+	public String confirmView(Form f) {
+		return "confirm2";
 	}
 
 }
---
+++ src/main/java/com/example/form/Form.java
@@ -0,0 +1,16 @@
+package com.example.form;
+
+import java.time.LocalDate;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import lombok.Data;
+
+@Data
+public class Form {
+	private String name;
+	private Integer age;
+	@DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+	private LocalDate birth;
+
+}
